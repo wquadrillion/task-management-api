a0bc03651601b5f3a89e276bb6256007843a0284
Olawale Qudrillion <mac@Olawale-Quadrillion.local>
2024-05-17 12:01:37 +0100

    minor correction

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,29 +4,28 @@ import { Repository } from 'typeorm';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
-import { TasksGateway } from './tasks.gateway';
 
 @Injectable()
 export class TasksService {
   constructor(
     @InjectRepository(Task)
     private tasksRepository: Repository<Task>,
-    private tasksGateway: TasksGateway,
   ) {}
 
   async create(createTaskDto: CreateTaskDto, userId: number): Promise<Task> {
     const task = this.tasksRepository.create({ ...createTaskDto, userId });
     const savedTask = await this.tasksRepository.save(task);
-    this.tasksGateway.server.emit('taskCreated', savedTask); // Emit event
     return savedTask;
+  }
+
+  async findAll(userId: number): Promise<Task[]> {
+    return this.tasksRepository.find({ where: { userId } });
   }
 
   async findOne(id: number, userId: number): Promise<Task> {
     const task = await this.tasksRepository.findOne({ where: { id, userId } });
     if (!task) {
-      throw new NotFoundException(
-        `Task with ID ${id} not found or you do not have access to it`,
-      );
+      throw new NotFoundException(`Task with ID ${id} not found`);
     }
     return task;
   }
@@ -44,11 +43,6 @@ export class TasksService {
   async remove(id: number, userId: number): Promise<{ message: string }> {
     const task = await this.findOne(id, userId);
     await this.tasksRepository.remove(task);
-    this.tasksGateway.server.emit('taskDeleted', { id }); // Emit event
     return { message: `Task with ID ${id} has been successfully deleted` };
-  }
-
-  async findAll(userId: number): Promise<Task[]> {
-    return this.tasksRepository.find({ where: { userId } });
   }
 }
